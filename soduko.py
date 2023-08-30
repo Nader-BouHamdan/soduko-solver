447189c09936aff24d7bc03af2bde7eb9a1f7417
Nader BouHamdan <nader.bouhamdan@gmail.com>
2023-08-30 14:07:43 +0300

    practice 1

---
+++ soduko.py
@@ -0,0 +1,39 @@
+# THIS IS A SODUKO SOLVER ALGORITHM
+# THE MAIN IDEA IS TO BACKTRACK
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+]
+
+def print_board(board):
+
+    for row in range(len(board)):
+        if row % 3 and row != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for column in range(len(board[0])):
+            if column % 3 == 0 and column != 0:
+                print(" | ", end="")
+
+            if column == 0:
+                print(board[row][column])
+            else:
+                print(str(board[row][column]) + " ")
+
+
+def find_empty(board):
+    for row in range(len(board)):
+        for column in range(len(board[0])):
+            if board[row][column] == 0:
+                return (row, column)
+
+
+print_board(board)

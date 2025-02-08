995c3ac6f5211101faa8aa40dee7fdaef0af403a
Nader BouHamdan <nader.bouhamdan@gmail.com>
2025-02-07 22:39:36 -0800

    full_code

--- soduko.py
+++ soduko.py
@@ -1,44 +1,82 @@
-# THIS IS A SODUKO SOLVER ALGORITHM
-# THE MAIN IDEA IS TO BACKTRACK
-
 board = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
 ]
 
-def print_board(board):
 
-    for row in range(len(board)):
-        if row % 3 and row != 0:
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
 
-        for column in range(len(board[0])):
-            if column % 3 == 0 and column != 0:
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
-            if column == 0:
-                print(board[row][column])
+            if j == 8:
+                print(bo[i][j])
             else:
-                print(str(board[row][column]) + " ")
+                print(str(bo[i][j]) + " ", end="")
 
 
-def find_empty(board):
-    for row in range(len(board)):
-        for column in range(len(board[0])):
-            if board[row][column] == 0:
-                return (row, column)
-            
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
 
-def hello(das):
-    print(das)
-
+    return None
 
 print_board(board)
-print('nader')
+solve(board)
+print("___________________")
+print_board(board)
